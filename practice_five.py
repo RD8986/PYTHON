15cc43e1bfa57aec71fb0e05b26fd7e87315d7aa
RD8986 <ritikdubey0@gmail.com>
2024-07-13 17:33:29 +0530

    PRACTICE 5

---
+++ practice_five.py
@@ -0,0 +1,13 @@
+# PRACTICE 5
+# 1. Create a tuple with three elements: a string, an integer, and a float.
+# 2. Unpack the tuple into three variables.
+# 3. Print each variable.
+
+mytuple=(1,"Amit",1.25)
+a,b,c=mytuple
+print("\n First variable : ")
+print(a)
+print("\n Two variable : ")
+print(b)
+print("\n Third variable : ")
+print(c)
