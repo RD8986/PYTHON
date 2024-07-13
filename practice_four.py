c3aa098fbbde503e0a8eeb1154b00223b22a44bb
RD8986 <ritikdubey0@gmail.com>
2024-07-13 17:28:53 +0530

    PRACTICE 4

---
+++ practice_four.py
@@ -0,0 +1,12 @@
+# PRACTICE 4
+# 1. Create a tuple containing the names of five continents.
+# 2. Print the third continent in the tuple.
+# 3. Try to change the second continent and observe the result.
+
+fivecontinents=("Africa", "Asia", "Europe", "North America", "South America")
+print(fivecontinents[2])
+
+# fivecontinents[2]=["Australia"]
+# print(fivecontinents)
+
+# TypeError: 'tuple' object does not support item assignment
