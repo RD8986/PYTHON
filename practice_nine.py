826f2a5a0f2d2aa39fca655fce7f5b8ed0894359
RD8986 <ritikdubey0@gmail.com>
2024-07-15 16:06:39 +0530

    PRACTICE NINE

---
+++ practice_nine.py
@@ -0,0 +1,19 @@
+# # PRACTICE 9
+# 1. Create a set of vowels.
+# 2. Check if the letter "e" is in the set.
+# 3. Check if the letter "x" is in the set.
+# 4. Print the results.
+
+
+vowels = {"a", "e", "i", "o", "u"}
+print(vowels)
+
+if "e" in vowels:
+    print(" E is in the set of vowels.")
+else:
+    print("E is not in the set of vowels.")
+
+if "x" in vowels:
+    print("X is in the set of vowels.")
+else:
+    print("X is not in the set of vowels.")
